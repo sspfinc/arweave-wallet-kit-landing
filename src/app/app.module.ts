e5942a57cc1ed3f2475c23dbfd6bbce3fad7effb
Peter Farber <farberpete@gmail.com>
2023-08-22 14:37:30 -0400

    Fixed: Importing Arweave Kit From Local Path..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ArweaveWalletKitNgModule } from '../../../arweave-wallet-kit-ng/projects/arweave-wallet-kit-ng/src/lib/arweave-wallet-kit-ng.module';
-import { AWKConnectButtonModule } from '../../../arweave-wallet-kit-ng/projects/arweave-wallet-kit-ng/src/lib/components/connect-button/connect-button.module';
-import { AWKProfileModalModule } from '../../../arweave-wallet-kit-ng/projects/arweave-wallet-kit-ng/src/lib/components/profile-modal/profile-modal.module';
-import { AWKConnectionModalModule } from '../../../arweave-wallet-kit-ng/projects/arweave-wallet-kit-ng/src/lib/components/connection-modal/connection-modal.module';
+import {
+  AWKConnectButtonModule,
+  AWKConnectionModalModule,
+  AWKProfileModalModule,
+  ArweaveWalletKitNgModule,
+} from 'arweave-wallet-kit-ng';
 
 @NgModule({
   declarations: [AppComponent],
